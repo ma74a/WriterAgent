9b1ee6e5d0db745c53c8f278b1b5f5ea110536d4
mahmoud etman <metman835@gmail.com>
2026-09-06 07:35:35 +0300

    updating main file to save the output into html and markdown files

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from app.graph import build_graph
+from app.output import save_html, save_markdown
 
 app = build_graph()
 
+
 user_prompt = """
-Write a beginner-friendly blog post explaining
-Retrieval-Augmented Generation with Python examples.
-Include diagrams and make it around 1500 words.
+Write a technical but beginner-friendly blog post explaining
+how to build a YOLO project using python.
+
+Include practical code examples, explain the architecture,
+and include diagrams where useful.
+
+The article should be approximately 2000 words.
 """
 
-result = app.invoke({"user_prompt": user_prompt})
 
-print(result)
+result = app.invoke({
+    "user_prompt": user_prompt
+})
+
+
+save_markdown(
+    content=result["final_blog"]
+)
+save_html(
+    content=result["final_blog"]
+)
