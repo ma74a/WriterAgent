4863bdbd3249f72e1e72cdc0f8bc1e2532dd7695
mahmoud etman <metman835@gmail.com>
2026-09-07 01:30:52 +0300

    adding researcher node

--- app/graph.py
+++ app/graph.py
@@ -5,6 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from langgraph.graph import StateGraph, START, END
 from app.state import BlogState
 from app.nodes.analyzer import prompt_analyzer
+from app.nodes.researcher import web_searcher
 from app.nodes.planner import blog_planner
 from app.nodes.content import content_generator
 from app.nodes.code import code_generator
@@ -12,10 +13,12 @@ from app.nodes.images import image_handler
 from app.nodes.assembler import blog_assembler
 
 
+
 def build_graph():
     graph = StateGraph(BlogState)
 
     graph.add_node("prompt_analyzer", prompt_analyzer)
+    graph.add_node("web_searcher", web_searcher)
     graph.add_node("blog_planner", blog_planner)
     graph.add_node("content_generator", content_generator)
     graph.add_node("code_generator", code_generator)
@@ -23,7 +26,8 @@ def build_graph():
     graph.add_node("blog_assembler", blog_assembler)
     
     graph.add_edge(START, "prompt_analyzer")
-    graph.add_edge("prompt_analyzer", "blog_planner")
+    graph.add_edge("prompt_analyzer", "web_searcher")
+    graph.add_edge("web_searcher", "blog_planner")
     graph.add_edge("blog_planner", "content_generator")
     graph.add_edge("blog_planner", "code_generator")
     graph.add_edge("blog_planner", "image_handler")
@@ -34,10 +38,10 @@ def build_graph():
 
     workflow = graph.compile()
     # png_data = workflow.get_graph().draw_mermaid_png()
-    # with open("graph_parallelly_with_assembler.png", "wb") as f:
+    # with open("graph_parallelly_with_assembler_and_web_searcher.png", "wb") as f:
     #     f.write(png_data)
 
     return workflow
 
 
-build_graph()
+# build_graph()
