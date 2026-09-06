bb3ab5a493cd74a706674d4ccc5515d32e6a1e63
mahmoud etman <metman835@gmail.com>
2026-09-06 04:01:16 +0300

    updating analyzer to use llm

--- app/nodes/analyzer.py
+++ app/nodes/analyzer.py
@@ -1,19 +1,38 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from app.state import BlogState
+from app.schemas import PromptAnalysis
+from app.llm import llm
 
+analyzer_llm = llm.with_structured_output(
+    PromptAnalysis
+)
 
 def prompt_analyzer(state: BlogState):
-    prompt = state["user_prompt"]
+    user_prompt = state["user_prompt"]
 
-    print(f"prompt: {prompt}")
+    system_prompt = """
+        You are the Prompt Analyzer for a professional AI Writer Agent.
 
-    # Temporary result
-    analysis = {
-        "topic": "Retrieval-Augmented Generation",
-        "audience": "beginners",
-        "tone": "educational",
-        "needs_code": True,
-        "needs_images": True,
-        "word_count": 1500,
+        Analyze the user's request for a blog post.
+
+        Determine:
+
+        1. The main topic
+        2. The target audience
+        3. The desired tone
+        4. Whether code examples are required
+        5. Whether images or diagrams are useful
+        6. The approximate desired word count
+
+        Do not write the blog post.
+
+        Only analyze the request and return the structured analysis.
+        """
+    result = llm.invoke([
+        SystemMessage(content=system_prompt),
+        HumanMessage(content=user_prompt),
+    ])
+
+    return {
+        "analysis": result # returns a PromptAnalysis object,
     }
-
-    return analysis
